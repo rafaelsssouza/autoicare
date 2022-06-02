a149847c5d04f67b51286e5e23c68561c398b596
Rafael Souza <94587686+rafaelsssouza@users.noreply.github.com>
2022-06-02 17:16:08 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
-import time
+from time import sleep
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import  By
 
+chaves = open("userpass.txt", "r")
+user = chaves.readline()
+senha = chaves.readline()
 
 class login:
     def __init__(self):
@@ -23,32 +26,31 @@ class login:
                 "arrow":{
                     "xpath":"/html/body/app-root/app-login-page/div/div/div[2]/button/fa-icon"
                 }
-            },
-            "filial":{
-                "xpath":"",
-                "css":""
             }
         }
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         
     def abrir_site(self):
-        time.sleep(2)
+        sleep(2)
         self.driver.get(self.site_link)
-        time.sleep(2)
+        sleep(2)
 
-    def usuario(self):
-        self.driver.find_element(By.ID, self.site_map["campos"]["username"]["id"]).send_keys()
+    def usuario(self): #usuario
+        self.driver.find_element(By.ID, self.site_map["campos"]["username"]["id"]).send_keys(user)
         
-    def senha(self):
-        self.driver.find_element(By.ID, self.site_map["campos"]["password"]["id"]).send_keys()
+    def senha(self): #senha
+        self.driver.find_element(By.ID, self.site_map["campos"]["password"]["id"]).send_keys(senha)
         
     def olhinho(self):
         self.driver.find_element(By.XPATH, self.site_map["button"]["eye"]["xpath"]).click()   
         
     def botaoiniciar(self):
         self.driver.find_element(By.XPATH, self.site_map["button"]["arrow"]["xpath"]).click()
-
+        
+    def selecionar_filial(self):
+        sleep(10)
+        self.driver.find_element(By.CSS_SELECTOR, ':nth-child(3) > .item > span').click()
 
 class pagelogin():
     url = login()
@@ -56,4 +58,5 @@ class pagelogin():
     url.usuario()
     url.senha()
     url.botaoiniciar()
+    url.selecionar_filial()
 
